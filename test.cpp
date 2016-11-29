95287f577c79d3f15609353e034910b2800a53f7
KerryHolmes <kjh80@zips.uakron.edu>
2016-11-28 19:33:01 -0500

    add couts

--- test.cpp
+++ test.cpp
@@ -6,23 +6,25 @@
 int main()
 {
 
-  std::vector<std::vector<double>> input(7018,std::vector<double>(10,0);
+  std::vector<std::vector<double>> input(1018,std::vector<double>(10,0));
   std::ifstream reader("./training.dat");
   std::string current;
-                                      
+
   for(int j = 0; j < input.size(); ++j)
   {
     for(int i = 0; i < 9; ++i)
     {
-      getline(reader, current, ",");
+      getline(reader, current, ',');
       input[j][i] = std::stoi(current);
     }
     getline(reader, current);
     input[j][9] = std::stod(current);
   }
 
-NeuralNetwork brain(10, 13, 3);
-  int max_epochs = 1000;
+std::cout << "Read" << std::endl;
+
+NeuralNetwork brain(7, 9, 3);
+  int max_epochs = 100;
   auto weights = brain.make_a_man_out_of_you(input, max_epochs);
   for(auto v : weights)
      std::cout << v << std::endl;
