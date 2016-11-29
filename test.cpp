22e2e5c05a6e2f4cf5a20ac2600fffea4d3efdc0
KerryHolmes <kjh80@zips.uakron.edu>
2016-11-28 19:21:35 -0500

    Attempt training

--- test.cpp
+++ test.cpp
@@ -6,21 +6,26 @@
 int main()
 {
 
-  std::vector<std::vector<int>> input;
-  
-  std::ifstream reader("");
-  
-  while(!reader.eof())
+  std::vector<std::vector<double>> input(7018,std::vector<double>(10,0);
+  std::ifstream reader("./training.dat");
+  std::string current;
+                                      
+  for(int j = 0; j < input.size(); ++j)
   {
     for(int i = 0; i < 9; ++i)
     {
-    int current;
-       getline(reader, current);
-       
-       
+      getline(reader, current, ",");
+      input[j][i] = std::stoi(current);
+    }
+    getline(reader, current);
+    input[j][9] = std::stod(current);
+  }
 
-
-NeuralNetwork brain()
+NeuralNetwork brain(10, 13, 3);
+  int max_epochs = 1000;
+  auto weights = brain.make_a_man_out_of_you(input, max_epochs);
+  for(auto v : weights)
+     std::cout << v << std::endl;
 
 return 0;
 }
