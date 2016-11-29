f8593d2c41763515f0061fc8d790ae1991512ddf
KerryHolmes <kjh80@zips.uakron.edu>
2016-11-28 19:10:18 -0500

    Start stub for test

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include "neural_network.hpp"
+#include <vector>
+
+int main()
+{
+
+  std::vector<std::vector<int>> input;
+  
+  std::ifstream reader("");
+  
+  while(!reader.eof())
+  {
+    for(int i = 0; i < 9; ++i)
+    {
+    int current;
+       getline(reader, current);
+       
+       
+
+
+NeuralNetwork brain()
+
+return 0;
+}
